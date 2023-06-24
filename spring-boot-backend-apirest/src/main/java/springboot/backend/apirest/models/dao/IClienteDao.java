1e09a40c143ba8254edbc1a8f1c5346bf1abf371
Anthony <anthonyricardoperez123@gmail.com>
2023-06-24 13:15:03 -0300

    spring boot backend rest

---
+++ spring-boot-backend-apirest/src/main/java/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -0,0 +1,9 @@
+package springboot.backend.apirest.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteDao extends CrudRepository<Cliente, Long>{
+
+}
---
+++ spring-boot-backend-apirest/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Anthony', 'Ricardo', 'anthony@gmail.com', '2023-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Andy', 'Garcia', 'andy@gmail.com', '2023-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Graciela', 'Perez', 'chela@gmail.com', '2023-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Gloria', 'Rodriguez', 'glo@gmail.com', '2023-01-01');
